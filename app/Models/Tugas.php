11e4f6be9a3222822c5ffe6974da28fc82a862c1
defarrel <Defarrel>
2025-06-26 21:00:36 +0700

    Membuat TugasController, model dan menambahkan routes tugas

--- app/Models/Tugas.php
+++ app/Models/Tugas.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tugas extends Model
 {
-    //
+    protected $fillable = [
+        'judul',
+        'deskripsi',
+    ];
+
+    public function guru()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
 }
+
